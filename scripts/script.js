47d6c3e39e6f85e5eacc10c2d1cb17a7a7d221d0
Joy Sóuza <joysouza.contato@gmail.com>
2024-07-11 19:12:19 -0300

    Adicionando slide carousel nas habilidades

--- scripts/script.js
+++ scripts/script.js
@@ -20,3 +20,38 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+// Carousel
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 7, // Quantidade de slides a serem exibidos inicialmente
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        loop: true, // Habilita o modo loop
+        autoplay: {
+            delay: 2000, // 3 segundos entre as transições
+            disableOnInteraction: false, // Continue o autoplay após interação
+        },
+        breakpoints: {
+            // Quando a largura da tela for 1025 pixels ou mais
+            1025: {
+                slidesPerView: 7,
+            },
+            // Quando a largura da tela for 769 pixels até 1024 pixels
+            769: {
+                slidesPerView: 5,
+            },
+            // Quando a largura da tela for 481 pixels até 768 pixels
+            481: {
+                slidesPerView: 3,
+            },
+            // Quando a largura da tela for 480 pixels ou menos
+            0: {
+                slidesPerView: 2,
+            }
+        }
+    });
+});
