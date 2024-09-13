525c351da2db83c644a620433ef635efd0e265f5
Joy Sóuza <joysouza.contato@gmail.com>
2024-09-13 00:21:55 -0300

    fix(imagens): mover imagens para o diretório public

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         <main>
             <header className='header' id='header'>
                 <a href='#' className='header__logo'>
-                    <img src='src/assets/logotipo/logotipo.svg' alt="Joy Sóuza" width="70" height="50" />
+                    <img src='/logotipo/logotipo.svg' alt="Joy Sóuza" width="70" height="50" />
                 </a>
                 <nav id="nav">
                     <button id="btn-mobile">
@@ -13,7 +13,7 @@ function Header() {
                     </button>
                     <ul className="header__menu" id="menu">
                         <a id="header__menu_logo">
-                            <img src="src/assets/logotipo/logotipo.svg" alt="Joy Sóuza" width="70" height="50" />
+                            <img src="/logotipo/logotipo.svg" alt="Joy Sóuza" width="70" height="50" />
                         </a>
                         <button id="close-menu">X</button>
                         <li><a href="#projetos">Projetos</a></li>
@@ -26,7 +26,7 @@ function Header() {
                 </nav>
             </header>
             <div className='introducao'>
-                <img src='src/assets/img_avatar.webp' className='introducao__img' alt="Foto de Perfil - Joy Sóuza" width="140" />
+                <img src='/img_avatar.webp' className='introducao__img' alt="Foto de Perfil - Joy Sóuza" width="140" />
                 <h1 className='introducao__title'>Olá! Sou a <strong>Joy Sóuza</strong></h1>
                 <h2 className='introducao__subtitle'>Desenvolvedora Front-End<strong>.</strong></h2>
             </div>
@@ -34,27 +34,27 @@ function Header() {
                 <ul className='contatos__lista'>
                     <li>
                         <a href="https://api.whatsapp.com/send?phone=5511969838280" target="_blank"><img
-                            src="src/assets/contato/whatsapp.svg" alt="WhatsApp" width="50" height="50" />
+                            src="/contato/whatsapp.svg" alt="WhatsApp" width="50" height="50" />
                         </a>
                     </li>
                     <li>
                         <a href="mailto:joysouza.contato@gmail.com?subject=Olá%20Joy Sóuza" target="_blank"><img
-                            src="src/assets/contato/email.svg" alt="E-mail" width="50" height="50" />
+                            src="/contato/email.svg" alt="E-mail" width="50" height="50" />
                         </a>
                     </li>
                     <li>
-                        <a href="https://github.com/jooysoouzaa" target="_blank"><img src="src/assets/contato/github.svg"
+                        <a href="https://github.com/jooysoouzaa" target="_blank"><img src="/contato/github.svg"
                             alt="Github" width="50" height="50" />
                         </a>
                     </li>
                     <li>
                         <a href="https://www.linkedin.com/in/jooyaraujo/" target="_blank" ><img
-                            src="src/assets/contato/linkedin.svg" alt="linkedin" width="50" height="50" />
+                            src="/contato/linkedin.svg" alt="linkedin" width="50" height="50" />
                         </a>
                     </li>
                     <li>
-                        <a href="src/assets/contato/curriculo_joyceDeSouzaAraujo_desenvolvedoraFrontEnd.pdf" target="_blank" ><img
-                            src="src/assets/contato/curriculo_icon.svg" alt="linkedin" width="50" height="50" />
+                        <a href="/contato/curriculo_joyceDeSouzaAraujo_desenvolvedoraFrontEnd.pdf" target="_blank" ><img
+                            src="/contato/curriculo_icon.svg" alt="linkedin" width="50" height="50" />
                         </a>
                     </li>
                 </ul>
