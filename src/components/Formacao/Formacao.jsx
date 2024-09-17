7c15d52a12308bfb574d8390c3d372561371dd8b
Joy Sóuza <joysouza.contato@gmail.com>
2024-09-17 14:56:48 -0300

    Fix: Convertendo o componente Formação de CSS para Styled Components

--- src/components/Formacao/Formacao.jsx
+++ src/components/Formacao/Formacao.jsx
@@ -1,56 +1,55 @@
-import './FormacaoStyle.css'
-
+import {
+    FormacaoSection, FormacaoTitulo, FormacaoTexto, FormacaoContainer, FormacaoFaculdade, FormacaoFaculdadeCurso,
+    FormacaoFaculdadeTipo, FormacaoFaculdadeConclusao, FormacaoExtra, FormacaoExtraCursos, Idiomas
+} from './FormacaoStyle';
 
 function Formacao() {
     return (
-        <section className='formacao' id='formacao' aria-label="Formação">
-            <div className='container'>
-                <h2 className="subtitulo">Formação</h2>
-                <p className="formacao__texto">
-                    Sou formada em Análise e Desenvolvimento de Sistemas pela FIAP desde dezembro de 2020. Em agosto de
-                    2024, concluí a formação como Engenheira Front-End pela EBAC. Atualmente, estou dedicando meus estudos em
-                    React, TypeScript e JavaScript, além de aperfeiçoar meu inglês.</p>
-                <div className="formacao__container">
-                    <div className="formacao__faculdade">
-                        <p className="formacao__faculdade-curso">Engenheiro Front-End</p>
-                        <h3 className="formacao__faculdade-tipo">Curso profissionalizante</h3>
-                        <ul className="formacao__faculdade-conclusao">
-                            <li>Ago/24</li>
-                            <li>EBAC</li>
-                        </ul>
-                    </div>
-                    <div className="formacao__faculdade">
-                        <p className="formacao__faculdade-curso">Análise e Desenvolvimento de Sistemas</p>
-                        <h3 className="formacao__faculdade-tipo">Ensino Superior</h3>
-                        <ul className="formacao__faculdade-conclusao">
-                            <li>Dez/20</li>
-                            <li>FIAP</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="formacao__extra">
-                    <div className="formacao__extra-cursos">
-                        <h3>Cursos</h3>
-                        <ul>
-                            <li>Formação Front-End (JS, React e TS)<span>Udemy - Cursando</span></li>
-                            <li>Jornada Full Stack - Clone do Tiktok<span>Ebac - 2023</span></li>
-                            <li>HTML e CSS<span>Origamid - 2023</span></li>
-                            <li>CSS Grid Layout<span>Origamid - 2020 </span></li>
-                            <li>CSS com SASS<span>Origamid - 2020</span></li>
-                            <li>Web Design<span>Origamid - 2020</span></li>
-                            <li>CSS Flexbox<span>Origamid - 2020</span></li>
-                            <li>Figma<span>Udemy - 2020</span></li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="idiomas">
-                    <h3>Idiomas</h3>
+        <FormacaoSection id='formacao' aria-label="Formação">
+            <FormacaoTitulo>Formação</FormacaoTitulo>
+            <FormacaoTexto>
+                Sou formada em Análise e Desenvolvimento de Sistemas pela FIAP desde dezembro de 2020. Em agosto de
+                2024, concluí a formação como Engenheira Front-End pela EBAC. Atualmente, estou dedicando meus estudos em
+                React, TypeScript e JavaScript, além de aperfeiçoar meu inglês.</FormacaoTexto>
+            <FormacaoContainer>
+                <FormacaoFaculdade>
+                    <FormacaoFaculdadeCurso>Engenheiro Front-End</FormacaoFaculdadeCurso>
+                    <FormacaoFaculdadeTipo>Curso profissionalizante</FormacaoFaculdadeTipo>
+                    <FormacaoFaculdadeConclusao>
+                        <li>Ago/24</li>
+                        <li>EBAC</li>
+                    </FormacaoFaculdadeConclusao>
+                </FormacaoFaculdade>
+                <FormacaoFaculdade>
+                    <FormacaoFaculdadeCurso>Análise e Desenvolvimento de Sistemas</FormacaoFaculdadeCurso>
+                    <FormacaoFaculdadeTipo>Ensino Superior</FormacaoFaculdadeTipo>
+                    <FormacaoFaculdadeConclusao>
+                        <li>Dez/20</li>
+                        <li>FIAP</li>
+                    </FormacaoFaculdadeConclusao>
+                </FormacaoFaculdade>
+            </FormacaoContainer>
+            <FormacaoExtra>
+                <FormacaoExtraCursos>
+                    <h3>Cursos</h3>
                     <ul>
-                        <li>Inglês<span> - Básico (Cursando)</span></li>
+                        <li>Front-End (JS, React e TS)<span>Udemy - Cursando</span></li>
+                        <li>HTML e CSS<span>Origamid - 2023</span></li>
+                        <li>CSS Grid Layout<span>Origamid - 2020 </span></li>
+                        <li>CSS com SASS<span>Origamid - 2020</span></li>
+                        <li>Web Design<span>Origamid - 2020</span></li>
+                        <li>CSS Flexbox<span>Origamid - 2020</span></li>
+                        <li>Figma<span>Udemy - 2020</span></li>
                     </ul>
-                </div>
-            </div>
-        </section>
+                </FormacaoExtraCursos>
+            </FormacaoExtra>
+            <Idiomas>
+                <h3>Idiomas</h3>
+                <ul>
+                    <li>Inglês<span> - Básico (Cursando)</span></li>
+                </ul>
+            </Idiomas>
+        </FormacaoSection>
     )
 }
 
---
+++ src/components/Formacao/FormacaoStyle.js
@@ -0,0 +1,199 @@
+import styled from 'styled-components'
+
+export const FormacaoSection = styled.section`
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 20px 60px;
+
+    @media screen and (max-width: 767px) {
+        grid-template-columns: 2fr;
+        gap: 40px;
+    }
+
+    @media screen and (max-width: 1023px) {
+        padding: 20px;
+}
+`;
+
+export const FormacaoTitulo = styled.h2`
+    color: #fef5f5;
+    margin-top: 100px;
+    margin-bottom: 40px;
+    font-size: 40px;
+    font-weight: 700;
+    font-family: "Titillium Web", sans-serif;
+`;
+
+export const FormacaoTexto = styled.p`
+    font-size: 1.125rem;
+    line-height: 1.4;
+    color: #bdbdbd;
+    padding-bottom: 20px;
+
+    @media screen and (max-width: 767px) {
+        font-size: 1rem;
+        line-height: 1.4;
+        color: #fef5f5;
+}
+`;
+
+export const FormacaoContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 20px;
+
+    @media screen and (max-width: 767px) {
+        grid-template-columns: 1fr;
+        
+        &> :nth-child(3) {
+            grid-row: initial;
+            grid-column: initial;
+    }
+}
+
+    @media only screen and (min-width: 768px) and (max-width: 1023px) {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 20px;
+    }
+
+    &> :nth-child(3) {
+        margin-bottom: 20px;
+        grid-row: 2;
+        grid-column: 1 / span 2;
+    }
+}
+`;
+
+export const FormacaoFaculdade = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px 20px;
+    padding: 20px;
+    background: #121212;
+    border: 1px solid #3d3d3d6e;
+    margin-bottom: 20px;
+    border-radius: 10px;
+    position: relative;
+
+    &::before {
+    content: "";
+    display: block;
+    width: 4px;
+    height: 50px;
+    background: linear-gradient(90deg, #FA12E3, #7312FA, #415CA7);
+    position: absolute;
+    top: 20px;
+    left: 0px;
+}
+    @media screen and (max-width: 400px) {
+        grid-template-columns: 1fr;
+}
+
+    @media screen and (max-width: 767px) {
+        margin-bottom: 0;
+        grid-template-columns: 1fr;
+}
+`;
+
+export const FormacaoFaculdadeCurso = styled.p`
+    font-size: 1.25rem;
+    line-height: 1.4;
+    background: linear-gradient(90deg, #FA12E3, #7312FA, #415CA7);
+    -webkit-background-clip: text;
+    color: transparent;
+    font-weight: 700;
+
+    @media screen and (max-width: 767px) {
+        font-size: 1rem;
+}
+`;
+
+export const FormacaoFaculdadeTipo = styled.h3`
+    font-size: 1.125rem;
+    letter-spacing: 1px;
+    line-height: 1.1;
+    font-weight: 400;
+    color: #fef5f5;
+
+    @media screen and (max-width: 767px) {
+        font-size: 1rem;
+        letter-spacing: 0.5px;
+}   
+`;
+
+export const FormacaoFaculdadeConclusao = styled.ul`
+    display: flex;
+    gap: 10px;
+    align-items: start;
+
+    li {
+    line-height: 1.4;
+    font-weight: 500;
+    border-radius: 4px;
+    background: #303030;
+    color: #fef5f5;
+    padding: 5px 10px;
+    font-size: 1.125rem;
+
+    @media screen and (max-width: 767px) {
+        font-size: 1rem;
+    }
+}
+`;
+
+export const FormacaoExtra = styled.div`
+    h3 {
+        line-height: 1.4;
+        color: #fef5f5;
+        text-transform: uppercase;
+        margin-bottom: 15px;
+        position: relative;
+
+        @media screen and (max-width: 767px) {
+            margin-top: 20px;
+}
+    }
+
+    li {
+    line-height: 1.1;
+    margin-bottom: 20px;
+    
+    @media screen and (max-width: 767px) {
+        font-size: 1rem;
+    }
+}
+    span {
+    color: #bdbdbd;
+    text-align: right;
+}
+`;
+
+export const FormacaoExtraCursos = styled.div`
+    li {
+        color: #bdbdbd;
+        display: flex;
+        justify-content: space-between;
+        font-size: 1.125rem;
+}
+`;
+
+export const Idiomas = styled.div`
+    h3 {
+        line-height: 1.4;
+        color: #fef5f5;
+        text-transform: uppercase;
+        margin-bottom: 15px;
+        position: relative;
+        margin-top: 30px;
+}
+    li {
+        color: #bdbdbd;
+        font-size: 1.125rem;
+        line-height: 1.1;
+
+        @media screen and (max-width: 767px) {
+        font-size: 1rem;
+    }
+    }
+`;
