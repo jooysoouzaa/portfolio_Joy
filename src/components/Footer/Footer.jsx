81721d533f2f4e564a8d7596f6619a3449677691
Joy Sóuza <joysouza.contato@gmail.com>
2024-09-17 16:36:48 -0300

    Fix: Convertendo o componente Footer de CSS para Styled Components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from 'react';
-import './FooterStyle.css';
+import {
+    FooterSection, FooterContainer, BtnTop, FooterImg, FooterCopy
+} from './FooterStyle';
 
 function Footer() {
     const anoAtual = new Date().getFullYear();
@@ -33,9 +35,9 @@ function Footer() {
     };
 
     return (
-        <section className='footer'>
-            <div className="footer__container">
-                <div className="btn-top" ref={btnTopRef}>
+        <FooterSection>
+            <FooterContainer>
+                <BtnTop ref={btnTopRef}>
                     <a href="#" onClick={scrollToTop} title="voltar ao topo">
                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
                             fill="currentColor">
@@ -43,13 +45,12 @@ function Footer() {
                                 d="m357-384 123-123 123 123 57-56-180-180-180 180 57 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
                         </svg>
                     </a>
-                </div>
-                <img src='/logotipo/logotipo.svg' className='footer_img' width="50" height="50" />
-                <p className="footer__copy"> &copy; {anoAtual}- Desenvolvido por <strong>Joy Sóuza</strong></p>
-            </div>
+                </BtnTop>
+                <FooterImg src='/logotipo/logotipo.svg' width="50" height="50" />
+                <FooterCopy> &copy; {anoAtual}- Desenvolvido por <strong>Joy Sóuza</strong></FooterCopy>
+            </FooterContainer>
             <div id="projeto02" ref={projetosSectionRef}></div>
-        </section>
+        </FooterSection>
     );
 }
-
 export default Footer;
---
+++ src/components/Footer/FooterStyle.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components'
+
+export const FooterSection = styled.section`
+    background-color: #010101;
+    margin-top: 100px;
+    border: 1px solid;
+    border-image: linear-gradient(90deg, #010101, #303030, #010101) 1;
+`;
+
+export const FooterContainer = styled.div`
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 12px 60px;
+
+    @media only screen and (max-width: 1023px) {
+        max-width: 1280px;
+        margin: 0 auto;
+        padding: 12px 20px;
+}
+`;
+
+export const BtnTop = styled.div`
+    a {
+        display: none;
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        width: 40px;
+        height: 40px;
+        background: linear-gradient(90deg, #FA12E3, #7312FA, #415CA7);
+        color: white;
+        border-radius: 50%;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        opacity: 0;
+        pointer-events: none;
+        transition: opacity 0.4s ease, transform 0.4s ease;
+        transform: translateY(40px);
+        cursor: pointer;
+    }
+
+    .show a {
+        display: flex;
+        opacity: 1;
+        z-index: 2;
+        pointer-events: auto;
+        transform: translateY(0);
+    }
+`;
+
+export const FooterImg = styled.img`
+    margin: 0 auto;
+    margin-bottom: 8px;
+`;
+
+export const FooterCopy = styled.p`
+    font-size: 1rem;
+    line-height: 1.33;
+    color: #fef5f5;
+    text-align: center;
+
+    strong {
+        background: linear-gradient(90deg, #FA12E3, #7312FA, #415CA7);
+        -webkit-background-clip: text;
+        color: transparent;
+    }
+`;
