7b0329a1cbadace69eefca95133920c15ded26d3
Joy Sóuza <joysouza.contato@gmail.com>
2024-09-17 11:55:28 -0300

    Fix: Convertendo o componente FaixaAnimada 0de CSS para Styled Components

--- src/components/FaixaAnimada/FaixaAnimada.jsx
+++ src/components/FaixaAnimada/FaixaAnimada.jsx
@@ -1,10 +1,12 @@
-import './FaixaAnimadaStyle.css';
+import {
+    Faixa, Faixa1, Faixa2, TextWrapper1, TextWrapper2
+} from './FaixaAnimadaStyle';
 
 const FaixaAnimada = () => {
     return (
-        <section className="faixa" aria-label="Faixa">
-            <div className="faixa1">
-                <div className="text-wrapper" id="text-wrapper1">
+        <Faixa>
+            <Faixa1>
+                <TextWrapper1>
                     <p>React</p>
                     <p>Styled Components</p>
                     <p>JavaScript</p>
@@ -19,13 +21,11 @@ const FaixaAnimada = () => {
                     <p>Bootstrap</p>
                     <p>Git</p>
                     <p>Figma</p>
-                    <p>APIs</p>
                     <p>React</p>
                     <p>Jquery</p>
                     <p>HTML</p>
                     <p>WordPress</p>
                     <p>CSS</p>
-                    <p>VueJS</p>
                     <p>TypeScript</p>
                     <p>Cypress</p>
                     <p>Styled Components</p>
@@ -40,22 +40,23 @@ const FaixaAnimada = () => {
                     <p>Grunt</p>
                     <p>Gulp</p>
                     <p>Parcel</p>
+                    <p>React</p>
                     <p>Bootstrap</p>
                     <p>Git</p>
+                    <p>Jest</p>
+                    <p>Vercel</p>
                     <p>Figma</p>
-                    <p>APIs</p>
                     <p>Jquery</p>
                     <p>HTML</p>
                     <p>WordPress</p>
                     <p>CSS</p>
-                    <p>VueJS</p>
                     <p>TypeScript</p>
                     <p>Cypress</p>
                     <p>Styled Components</p>
-                </div>
-            </div>
-            <div className="faixa2">
-                <div className="text-wrapper" id="text-wrapper2">
+                </TextWrapper1>
+            </Faixa1>
+            <Faixa2>
+                <TextWrapper2>
                     <p>React</p>
                     <p>Styled Components</p>
                     <p>JavaScript</p>
@@ -70,18 +71,17 @@ const FaixaAnimada = () => {
                     <p>Bootstrap</p>
                     <p>Git</p>
                     <p>Figma</p>
-                    <p>APIs</p>
+                    <p>React</p>
                     <p>Jquery</p>
                     <p>HTML</p>
                     <p>WordPress</p>
                     <p>CSS</p>
-                    <p>VueJS</p>
                     <p>TypeScript</p>
                     <p>Cypress</p>
                     <p>Styled Components</p>
                     <p>React</p>
-                    <p>Styled Components</p>
                     <p>JavaScript</p>
+                    <p>React</p>
                     <p>TypeScript</p>
                     <p>Cypress</p>
                     <p>Jest</p>
@@ -90,23 +90,22 @@ const FaixaAnimada = () => {
                     <p>Grunt</p>
                     <p>Gulp</p>
                     <p>Parcel</p>
+                    <p>React</p>
                     <p>Bootstrap</p>
                     <p>Git</p>
+                    <p>Jest</p>
+                    <p>Vercel</p>
                     <p>Figma</p>
-                    <p>APIs</p>
                     <p>Jquery</p>
                     <p>HTML</p>
                     <p>WordPress</p>
                     <p>CSS</p>
-                    <p>VueJS</p>
                     <p>TypeScript</p>
                     <p>Cypress</p>
                     <p>Styled Components</p>
-                </div>
-            </div>
-
-
-        </section>
+                </TextWrapper2>
+            </Faixa2>
+        </Faixa>
     );
 };
 
