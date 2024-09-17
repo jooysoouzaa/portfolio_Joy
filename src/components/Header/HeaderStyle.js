d1757bbe59dc49cf402a1b436b560a09a8bfded7
Joy Sóuza <joysouza.contato@gmail.com>
2024-09-17 10:59:26 -0300

    Fix: Convertendo o componente Header de CSS para Styled Components

--- src/components/Header/HeaderStyle.js
+++ src/components/Header/HeaderStyle.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 
-// Container principal com fundo diferente em dispositivos móveis
 export const MainContainer = styled.main`
     background-image: url(/bg_quadriculado.png);
     width: 100%;
@@ -13,8 +12,7 @@ export const MainContainer = styled.main`
         background-image: url(/bg_quadriculado_mobile.png);
         height: 520px;
     }
-`;
-
+    `;
 
 export const Container = styled.div`
     max-width: 1280px;
@@ -23,10 +21,9 @@ export const Container = styled.div`
 
     @media screen and (max-width: 1023px) {
         padding: 20px;
-    }
+}
 `;
 
-// Wrapper do cabeçalho com ajustes para telas menores
 export const HeaderWrapper = styled.header`
     display: flex;
     justify-content: space-between;
@@ -36,50 +33,17 @@ export const HeaderWrapper = styled.header`
     padding: 12px 40px;
     border-radius: 100px;
     z-index: 4;
+    `;
 
-    @media screen and (max-width: 1023px) {
-        padding: 8px 20px;
-    }
-
-    img {
-        width: 45px;
-        height: 45px;
-    }
-
-    @media screen and (max-width: 767px) {
-        display: flex;
-    }
-`;
-
-// Logo com borda arredondada
 export const Logo = styled.a`
-    border-radius: 10px;
-`;
-
-// Navegação com comportamento dinâmico para a exibição do menu
-export const Nav = styled.nav`
-    #nav.active #menu {
-        left: 0;
-        visibility: visible;
+    img{
+    width: 45px;
+    height: 45px;
     }
+    `;
 
-    @media screen and (max-width: 1023px) {
-        #nav.active #hamburger {
-            border-top-color: transparent;
-        }
+export const Nav = styled.nav``;
 
-        #nav.active #hamburger::before {
-            transform: rotate(135deg);
-        }
-
-        #nav.active #hamburger::after {
-            transform: rotate(-135deg);
-            top: -7px;
-        }
-    }
-`;
-
-// Botão móvel exibido apenas em telas menores
 export const ButtonMobile = styled.button`
     display: none;
     text-transform: uppercase;
@@ -95,9 +59,8 @@ export const ButtonMobile = styled.button`
     @media screen and (max-width: 1023px) {
         display: flex;
     }
-`;
+    `;
 
-// Hambúrguer do menu com transições suaves
 export const Hamburger = styled.span`
     display: block;
     border-top: 3px solid;
@@ -112,28 +75,9 @@ export const Hamburger = styled.span`
         margin-top: 5px;
         background-color: currentColor;
         transition: 0.3s;
-        position: relative;
     }
+    `;
 
-    @media screen and (max-width: 1023px) {
-        display: block;
-
-        &.active {
-            border-top-color: transparent;
-
-            &::before {
-                transform: rotate(135deg);
-            }
-
-            &::after {
-                transform: rotate(-135deg);
-                top: -7px;
-        }
-    }
-}
-`;
-
-// Menu com comportamento de slide para telas menores
 export const Menu = styled.ul`
     display: flex;
     gap: 10px;
@@ -151,41 +95,16 @@ export const Menu = styled.ul`
         transition: left 0.5s ease;
         overflow-y: auto;
     }
+    `;
 
-    a {
-        padding: 0 20px 20px 20px;
-        color: #ffffff;
-        text-decoration: none;
-        transition: background-color 0.3s;
-
-        &:hover {
-            background-color: transparent;
-        }
-    }
-
-    @media screen and (max-width: 1023px) {
-        &.active {
-            left: 0;
-            visibility: visible;
-        }
-    }
-`;
-
-// Logo do menu, visível apenas em telas menores
 export const MenuLogo = styled.a`
     display: none;
 
     @media screen and (max-width: 1023px) {
         display: block;
-        margin-top: 25px;
-
-        img {
-            margin-top: 25px;
-        }
     }
-`;
+    `;
 
-// Botão para fechar o menu, visível apenas em telas menores
 export const CloseMenuButton = styled.button`
     display: none;
 
@@ -201,9 +120,8 @@ export const CloseMenuButton = styled.button`
         font-size: 1.5rem;
         cursor: pointer;
     }
-`;
+    `;
 
-// Item de menu com estilos para links
 export const MenuItem = styled.li`
     a {
         font-size: 1rem;
@@ -217,22 +135,20 @@ export const MenuItem = styled.li`
         border-radius: 8px;
 
         &:hover {
-            background: #3d3d3d5b;
-            color: #fef5f5;
+        background: #3d3d3d5b;
+        color: #fef5f5;
         }
 
         @media screen and (max-width: 767px) {
-            padding: 10px 15px 10px 5px;
+        padding: 10px 15px 10px 5px;
         }
     }
-`;
+    `;
 
-// Introdução centralizada
 export const Introducao = styled.div`
     text-align: center;
-`;
+    `;
 
-// Imagem da introdução com ajuste de tamanho em telas menores
 export const IntroducaoImg = styled.img`
     max-width: 100%;
     width: 150px;
@@ -244,9 +160,8 @@ export const IntroducaoImg = styled.img`
         max-width: 100%;
         width: 120px;
     }
-`;
+    `;
 
-// Título da introdução com ajuste de tamanho em telas menores
 export const IntroducaoTitle = styled.h1`
     font-size: 3rem;
     font-weight: 400;
@@ -263,10 +178,9 @@ export const IntroducaoTitle = styled.h1`
     @media screen and (max-width: 767px) {
         font-size: 1.89rem;
         margin-top: 20px;
-    }
+}
 `;
 
-// Subtítulo da introdução com ajuste de tamanho em telas menores
 export const IntroducaoSubtitle = styled.h2`
     font-size: 2rem;
     font-weight: 500;
@@ -276,33 +190,28 @@ export const IntroducaoSubtitle = styled.h2`
     @media screen and (max-width: 767px) {
         font-size: 1.4rem;
     }
-`;
+    `;
 
-// Contatos centralizados
 export const Contatos = styled.div`
     display: flex;
     justify-content: center;
-`;
+    `;
 
-// Lista de contatos com ajuste de espaço em telas menores
 export const ContatosLista = styled.ul`
     display: flex;
     gap: 20px;
 
-    @media screen and (max-width: 360px) {
+    @media screen and (max-width: 480px){
         gap: 8px;
     }
 `;
 
-// Item de contato com ajuste de largura
 export const ContatoItem = styled.li`
     width: 50px;
-`;
+    `;
 
-// Link de contato (sem estilos adicionais por enquanto)
 export const ContatoLink = styled.a``;
 
-// Imagem de contato com efeito de zoom ao passar o mouse
 export const ContatoImg = styled.img`
     transition: transform 0.5s ease-out;
 
@@ -310,7 +219,7 @@ export const ContatoImg = styled.img`
         transform: scale(1.2);
     }
 
-    @media screen and (max-width: 360px) {
-        width: 40px;
+    @media screen and (max-width: 480px) {
+        width: 45px;
     }
-`;
+    `;
