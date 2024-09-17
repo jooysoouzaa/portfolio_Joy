0b098969f621b808e8f89e9194aa4d84abfc4242
Joy Sóuza <joysouza.contato@gmail.com>
2024-09-17 16:14:46 -0300

    Fix: Convertendo o componente Exériencias de CSS para Styled Components

--- src/components/Experiencias/Experiencias.jsx
+++ src/components/Experiencias/Experiencias.jsx
@@ -1,65 +1,66 @@
-import './ExperienciasStyle.css'
 
+import {
+    ExperienciasSection, ExperienciasTitulo, ExperienciasContainer, ExperienciasEmpresa,
+    ExperienciasSubtitulo, ExperienciasHabilidades, ExperienciasParagrafo
+} from './ExperienciasStyle';
 
 function Experiencias() {
     return (
-        <section className='experiencias' id='experiencias' aria-label="Experiência">
-            <div className='container'>
-                <h2 className="subtitulo">Experiências</h2>
-                <div className="experiencias__container ">
-                    <div className="experiencias__empresa">
-                        <h3 className="experiencias__titulo">Código Certo Coders - Dev. Front-End | Voluntária</h3>
-                        <ul className="experiencias__habilidades">
-                            <li>Ago/24 - Atualmente</li>
-                        </ul>
-                        <p className="experiencias__paragrafo ">Atuação como desenvolvedora front-end, utilizando React, CSS e Figma
-                            para criar interfaces responsivas e intuitivas. Participação em reuniões semanais para
-                            alinhamento das tarefas e acompanhamento do progresso dos projetos.
-                        </p>
-                    </div>
-                    <div className="experiencias__empresa">
-                        <h3 className="experiencias__titulo">Avioriz - Web Designer | Autonôma</h3>
-                        <ul className="experiencias__habilidades">
-                            <li>Jun/23 - Atualmente</li>
-                        </ul>
-                        <p className="experiencias__paragrafo ">Construção e manutenção de sites e sistemas low code no WordPress e
-                            Elementor, sempre enfatizando a experiência do usuário, alto desempenho, design moderno, facilidade de uso
-                            e carregamento rápido.
-                        </p>
-                    </div>
-                    <div className="experiencias__empresa">
-                        <h3 className="experiencias__titulo">Realizzart - Designer Gráfico | Autonôma</h3>
-                        <ul className="experiencias__habilidades">
-                            <li>Maio/21 - Mar/23</li>
-                        </ul>
-                        <p className="experiencias__paragrafo">Desenvolvimento de projetos de identidade visual, arte digital e
-                            monogramas através das ferramentas Adobe Illustrator e Photoshop para empreendedores de diversas
-                            áreas.</p>
-                    </div>
-                    <div className="experiencias__empresa">
-                        <h3 className="experiencias__titulo">T-Systems - Dev. front-end | Estágio</h3>
-                        <ul className="experiencias__habilidades">
-                            <li>Out/19 - Jan/20</li>
-                        </ul>
-                        <p className="experiencias__paragrafo ">Desenvolvimento e manutenção de projetos Web para a Daimler
-                            (Mercedes-Benz) utilizando HTML5, CSS3, Bootstrap3 e JavaScript.</p>
-                    </div>
-                </div>
-                {/* <div className="voluntario">
-                    <h3 className="voluntario__titulo">Trabalho Voluntário</h3>
-                    <div className="experiencias__empresa">
-                        <h3 className="experiencias__titulo">Código Certo Coders - Dev. Front-End</h3>
-                        <ul className="experiencias__habilidades">
-                            <li>Ago/24 - Atualmente</li>
-                        </ul>
-                        <p className="experiencias__habilidades">Atuação como desenvolvedora front-end, utilizando React, CSS e Figma
-                            para criar interfaces responsivas e intuitivas. Participação em reuniões semanais para
-                            alinhamento das tarefas e acompanhamento do progresso dos projetos.
-                        </p>
-                    </div>
-                </div> */}
-            </div>
-        </section>
+        <ExperienciasSection id='experiencias' aria-label="Experiência">
+            <ExperienciasTitulo>Experiências</ExperienciasTitulo>
+            <ExperienciasContainer>
+                <ExperienciasEmpresa>
+                    <ExperienciasSubtitulo>Código Certo Coders - Dev. Front-End | Voluntária</ExperienciasSubtitulo>
+                    <ExperienciasHabilidades>
+                        <li>Ago/24 - Atualmente</li>
+                    </ExperienciasHabilidades>
+                    <ExperienciasParagrafo>Atuação como desenvolvedora front-end, utilizando React, CSS e Figma
+                        para criar interfaces responsivas e intuitivas. Participação em reuniões semanais para
+                        alinhamento das tarefas e acompanhamento do progresso dos projetos.
+                    </ExperienciasParagrafo>
+                </ExperienciasEmpresa>
+                <ExperienciasEmpresa>
+                    <ExperienciasSubtitulo>Avioriz - Web Designer | Autonôma</ExperienciasSubtitulo>
+                    <ExperienciasHabilidades>
+                        <li>Jun/23 - Atualmente</li>
+                    </ExperienciasHabilidades>
+                    <ExperienciasParagrafo>Construção e manutenção de sites e sistemas low code no WordPress e
+                        Elementor, sempre enfatizando a experiência do usuário, alto desempenho, design moderno, facilidade de uso
+                        e carregamento rápido.
+                    </ExperienciasParagrafo>
+                </ExperienciasEmpresa>
+                <ExperienciasEmpresa>
+                    <ExperienciasSubtitulo>Realizzart - Designer Gráfico | Autonôma</ExperienciasSubtitulo>
+                    <ExperienciasHabilidades>
+                        <li>Maio/21 - Mar/23</li>
+                    </ExperienciasHabilidades>
+                    <ExperienciasParagrafo>Desenvolvimento de projetos de identidade visual, arte digital e
+                        monogramas através das ferramentas Adobe Illustrator e Photoshop para empreendedores de diversas
+                        áreas.</ExperienciasParagrafo>
+                </ExperienciasEmpresa>
+                <ExperienciasEmpresa>
+                    <ExperienciasSubtitulo>T-Systems - Dev. front-end | Estágio</ExperienciasSubtitulo>
+                    <ExperienciasHabilidades>
+                        <li>Out/19 - Jan/20</li>
+                    </ExperienciasHabilidades>
+                    <ExperienciasParagrafo>Desenvolvimento e manutenção de projetos Web para a Daimler
+                        (Mercedes-Benz) utilizando HTML5, CSS3, Bootstrap3 e JavaScript.</ExperienciasParagrafo>
+                </ExperienciasEmpresa>
+            </ExperienciasContainer>
+            {/* <Voluntario>
+                <VoluntarioTitulo>Trabalho Voluntário</VoluntarioTitulo>
+                <ExperienciasEmpresa>
+                    <ExperienciasSubtitulo>Código Certo Coders - Dev. Front-End</ExperienciasSubtitulo>
+                    <ExperienciasHabilidades>
+                        <li>Ago/24 - Atualmente</li>
+                    </ExperienciasHabilidades>
+                    <ExperienciasHabilidades>Atuação como desenvolvedora front-end, utilizando React, CSS e Figma
+                        para criar interfaces responsivas e intuitivas. Participação em reuniões semanais para
+                        alinhamento das tarefas e acompanhamento do progresso dos projetos.
+                    </ExperienciasHabilidades>
+                </ExperienciasEmpresa>
+            </Voluntario> */}
+        </ExperienciasSection>
     )
 }
 
---
+++ src/components/Experiencias/ExperienciasStyle.js
@@ -0,0 +1,125 @@
+import styled from 'styled-components'
+
+export const ExperienciasSection = styled.section`
+max-width: 1280px;
+    margin: 0 auto;
+    padding: 20px 60px;
+
+    @media screen and (max-width: 1023px) {
+        padding: 20px;
+    }
+        
+`;
+
+export const ExperienciasTitulo = styled.h2`
+    color: #fef5f5;
+    margin-top: 100px;
+    margin-bottom: 40px;
+    font-size: 40px;
+    font-weight: 700;
+    font-family: "Titillium Web", sans-serif;
+
+    @media screen and (max-width: 767px) {
+        font-size: 30px !important;
+        margin-top: 60px;
+        margin-bottom: 20px;
+}
+`;
+
+
+export const ExperienciasContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 2fr);
+    gap: 20px;
+
+    @media (max-width: 767px) {
+        grid-template-columns: 1fr;
+    }
+
+    @media only screen and (min-width: 768px) and (max-width: 991px) {
+        grid-template-columns: 1fr 1fr;
+}
+`;
+
+export const ExperienciasEmpresa = styled.div`
+    display: block;
+    padding: 20px;
+    background: #121212;
+    border: 1px solid #3d3d3d6e;
+    border-radius: 10px;
+    position: relative;
+
+    &::before {
+    content: "";
+    display: block;
+    width: 4px;
+    height: 50px;
+    background: linear-gradient(90deg, #FA12E3, #7312FA, #415CA7);
+    position: absolute;
+    top: 20px;
+    left: 0;
+}
+`;
+
+export const ExperienciasSubtitulo = styled.div`
+    font-size: 1.125rem;
+    margin-bottom: 10px;
+    line-height: 1.3;
+    font-weight: bold;
+    background: linear-gradient(90deg, #FA12E3, #7312FA, #415CA7);
+    -webkit-background-clip: text;
+    color: transparent;
+
+    @media (max-width: 767px){
+        font-size: 1rem;
+        letter-spacing: 0.5px;
+}
+`;
+
+export const ExperienciasHabilidades = styled.ul`
+    li {
+        display: inline-block;
+        line-height: 1.4;
+        font-weight: 500;
+        border-radius: 4px;
+        background: #303030;
+        color: #fef5f5;
+        padding: 5px 10px;
+        font-size: 1.125rem;
+
+        @media (max-width: 767px) {
+        font-size: 1rem;
+    }
+}
+`;
+
+export const ExperienciasParagrafo = styled.p`
+    display: flex;
+    gap: 10px;
+    margin: 10px 0px;
+    align-items: start;
+    font-size: 1.125rem;
+    line-height: 1.4;
+    color: #bdbdbd;
+`;
+
+
+export const Voluntario = styled.div`
+    margin-top: 40px;
+
+    li {
+    font-size: 1.125rem;
+    line-height: 1.1;
+}
+`;
+
+
+export const VoluntarioTitulo = styled.div`
+    line-height: 1.4;
+    color: #fef5f5;
+    text-transform: uppercase;
+    margin-bottom: 15px;
+    position: relative;
+    margin-top: 30px;
+    font-weight: 600;
+`;
